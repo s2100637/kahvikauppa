33e9f1eeb755b1cbe9506f03cdc39c778b3d91b7
s2100637 <peter.knutas@edu.bc.fi>
2022-02-04 10:57:29 +0200

    update README.md

---
+++ src/main/java/com/example/kahvikauppa/KahvikauppaApplication.java
@@ -0,0 +1,13 @@
+package com.example.kahvikauppa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KahvikauppaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KahvikauppaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kahvikauppa/KahvikauppaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kahvikauppa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KahvikauppaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
